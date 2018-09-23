a2bee1a462fc361367ad766c9ea988adfc55ebdf
Mahesh Chathuranga <mcha@tobii.com>
2018-09-23 13:58:19 +0200

    Add Location.java

---
+++ InstagramDataCollector/src/main/java/se/du/instagramdatacollector/dto/Location.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.du.instagramdatacollector.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author mcha
+ */
+@Entity
+public class Location {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    private City city;
+    private String name;
+    private String url;
+
+    public Location(City city, String name, String url) {
+        this.city = city;
+        this.name = name;
+        this.url = url;
+    }
+
+    public Location() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    
+}
